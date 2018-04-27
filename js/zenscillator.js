33956f2d1ab0c2e82921af4f4cf7a943e67fa1d8
zach kramer <zkramer22@gmail.com>
2018-04-27 17:17:44 -0400

    add preventDefault to all event listeners

--- js/zenscillator.js
+++ js/zenscillator.js
@@ -221,6 +221,8 @@ document.addEventListener('mouseout', (e) => {
 });
 
 document.addEventListener('click', (e) => {
+  e.preventDefault();
+  
   if (e.target.className === 'instrument') {
     const type = (e.target.id);
     switch (type) {
@@ -301,6 +303,8 @@ document.addEventListener('click', (e) => {
 });
 
 document.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  
   if (e.target.className === 'natural' || e.target.className === 'flat') {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -332,6 +336,8 @@ document.addEventListener('mousedown', (e) => {
 });
 
 document.addEventListener('mouseup', (e) => {
+  e.preventDefault();
+  
   if (MOUSECODES.hasOwnProperty(e.target.id)) {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -354,6 +360,8 @@ document.addEventListener('mouseup', (e) => {
 //////////////////////
 
 document.addEventListener('touchstart', (e) => {
+  e.preventDefault();
+  
   if (e.target.className === 'natural' || e.target.className === 'flat') {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -385,6 +393,8 @@ document.addEventListener('touchstart', (e) => {
 });
 
 document.addEventListener('touchend', (e) => {
+  e.preventDefault();
+  
   if (MOUSECODES.hasOwnProperty(e.target.id)) {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -407,6 +417,8 @@ document.addEventListener('touchend', (e) => {
 //////////////////////
 
 document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  
   if (KEYCODES.hasOwnProperty(e.keyCode)) {
     if (e.repeat) { return null }
 
@@ -481,6 +493,8 @@ document.addEventListener('keydown', (e) => {
 });
 
 document.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  
   if (KEYCODES.hasOwnProperty(e.keyCode)) {
     const note = KEYCODES[e.keyCode][0];
     const color = KEYCODES[e.keyCode][1];
