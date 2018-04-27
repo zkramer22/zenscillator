1f679d9042961720797137100890a1af9182e77d
zach kramer <zkramer22@gmail.com>
2018-04-27 17:36:41 -0400

    webpack update bundle.js

--- bundle.js
+++ bundle.js
@@ -294,6 +294,8 @@ document.addEventListener('mouseout', (e) => {
 });
 
 document.addEventListener('click', (e) => {
+  e.preventDefault();
+  
   if (e.target.className === 'instrument') {
     const type = (e.target.id);
     switch (type) {
@@ -374,6 +376,8 @@ document.addEventListener('click', (e) => {
 });
 
 document.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  
   if (e.target.className === 'natural' || e.target.className === 'flat') {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -405,6 +409,8 @@ document.addEventListener('mousedown', (e) => {
 });
 
 document.addEventListener('mouseup', (e) => {
+  e.preventDefault();
+  
   if (MOUSECODES.hasOwnProperty(e.target.id)) {
     const note = e.target.id;
     const color = MOUSECODES[e.target.id][0];
@@ -421,11 +427,71 @@ document.addEventListener('mouseup', (e) => {
     }
   }
 });
+
+//////////////////////
+//// touch events ////
+//////////////////////
+
+document.addEventListener('touchstart', (e) => {
+  e.preventDefault();
+  
+  if (e.target.className === 'natural' || e.target.className === 'flat') {
+    const note = e.target.id;
+    const color = MOUSECODES[e.target.id][0];
+    const hex = MOUSECODES[e.target.id][1];
+
+    window.hexOn = hex;
+
+    if (window.gradient) {
+      if (window.gradient.length === 4) { window.gradient.pop() }
+      window.gradient.unshift(hexOn);
+    }
+    else {
+      window.gradient = [hexOn];
+    }
+
+    switch (note[0]) {
+      case "e":
+        instrument.triggerAttack(`${note.slice(1)}${octave + 1}`);
+        sustainClassToggle(`${note}`, `${color}`);
+        window.drawer = window.setInterval(drawLoop, 10);
+        break;
+      default:
+        instrument.triggerAttack(`${note}${octave}`);
+        sustainClassToggle(`${note}`, `${color}`);
+        window.drawer = window.setInterval(drawLoop, 10);
+        break;
+    }
+  }
+});
+
+document.addEventListener('touchend', (e) => {
+  e.preventDefault();
+  
+  if (MOUSECODES.hasOwnProperty(e.target.id)) {
+    const note = e.target.id;
+    const color = MOUSECODES[e.target.id][0];
+    const hex = MOUSECODES[e.target.id][1];
+
+    // turn off note //
+    if (note[0] === 'e') {
+      instrument.triggerRelease(`${note.slice(1)}${octave + 1}`);
+      sustainClassToggle(`${note}`, `${color}`);
+    }
+    else {
+      instrument.triggerRelease(`${note}${octave}`);
+      sustainClassToggle(`${note}`, `${color}`);
+    }
+  }
+});
+
 //////////////////////
 /// keyboard events //
 //////////////////////
 
 document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+  
   if (KEYCODES.hasOwnProperty(e.keyCode)) {
     if (e.repeat) { return null }
 
@@ -500,6 +566,8 @@ document.addEventListener('keydown', (e) => {
 });
 
 document.addEventListener('keyup', (e) => {
+  e.preventDefault();
+  
   if (KEYCODES.hasOwnProperty(e.keyCode)) {
     const note = KEYCODES[e.keyCode][0];
     const color = KEYCODES[e.keyCode][1];
